b03476ddecbb00868e0b0bc644053848e25402de
soodala2003 <soodala78@gmail.com>
2024-11-05 01:51:39 -0500

    updated login.js

--- routes/login.js
+++ routes/login.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("login");
+    //next();
 });
 
 router.post("/", (req, res) => {
     res.send(`Success: Your username is ${req.body.username} and your password is ${req.body.password}!`);
+    //next();
 });
 
 module.exports =router;
