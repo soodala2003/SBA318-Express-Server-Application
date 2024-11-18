2cb8c6f6a2433e805add7b521fb9bc8dd171193e
soodala2003 <soodala78@gmail.com>
2024-11-18 02:41:14 -0500

    added comments

--- data/comments.js
+++ data/comments.js
@@ -1,4 +1,47 @@
-const comments = [];
+const comments = [
+    {
+      id: 1,
+      userId: 1,
+      postId: 1,
+      body:
+        "simiNisi do cillum labore deserunt aute.ique esse doloribus nihil accusamusomnis aut molestiae vel vero",
+    },
+    {
+      id: 2,
+      userId: 1,
+      postId: 2,
+      body:
+        "Non aliqua laborum commodo adipisicing commodo laboris consectetur est laborum sint."  
+    },
+    {
+      id: 3,
+      userId: 1,
+      postId: 3,
+      body:
+        "Cillum ad ea adipisicing eiusmod incididunt.Cupidatat in minim officia veniam incididunt exercitation."  
+    },
+    {
+      id: 4,
+      userId: 2,
+      postId: 4,
+      body:
+        "Lomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus perspiciatis cum ut laudantium",
+    },
+    {
+      id: 5,
+      userId: 2,
+      postId: 5,
+      body:
+        "Exercitation duis do nostrud elit enim nisi labore ut qui commodo. Commodo minim quis sit deserunt cillum."  
+    },
+    {
+      id: 6,
+      userId: 3,
+      postId: 7,
+      body:
+        "Aute in culpa esse sunt aliqua eu dolor aliqua pariatur laborum. Sint Lorem exercitation voluptate consequat in anim irure ipsum officia ea."  
+    },
+];
 
 module.exports = comments;
     
